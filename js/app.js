fab759bd8984e55b6a48559af5a516ee48fa26b1
lemarcantoine <markitos.anthos@gmail.com>
2016-09-29 14:45:08 +0200

    Fixed "no error message"

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ define( [ "ui", "compiler" ], function( UI, Compiler ) {
         ui.setOutput( command );
         ui.selectOutput();
       } catch( exception ) {
-        ui.setOutput( command );
+        ui.setOutput( exception );
       }
     } );
 
